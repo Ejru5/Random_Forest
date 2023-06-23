5a79a1bddb997578d59f8243475eb1f7f627f9b3
root <root@DHRUV>
2023-06-23 16:09:35 +0530

    SQL Added using SQLite

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,5 @@
-from typing import Union
-from fastapi import FastAPI,Header
+from typing import Union,Optional
+from fastapi import FastAPI,Header,HTTPException
 from pydantic import BaseModel
 from typing_extensions import Annotated
 from sqlmodel import SQLModel,create_engine,Field,Session,update
@@ -7,16 +7,19 @@ from sqlmodel import SQLModel,create_engine,Field,Session,update
 app = FastAPI()
 #postgressql://<username>:<password>@localhost/fastapi, localhost -> ip addrress/domain name
 #create user username with encryption password 'password'
-DATABASE_URL = 'postgresql://postgres:admin@localhost/fastapi'
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = 'postgresql://postgres:admin@localhost:5433/fastapi'
+# engine = create_engine(DATABASE_URL)
+DATABASE_URL = "sqlite:///./sql_app.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+# connect_args={"check_same_thread":False} only in sqlite not in postgres
 def creat_db_tables():
     SQLModel.metadata.create_all(engine)
 
-# class Users(SQLModel, table=True):
-#     id : Optional[int] = Field(default=None,primary_key = True)
-#     name : str
-#     city : str
-#     email : str
+class Users(SQLModel, table=True):
+    id : Optional[int] = Field(default=None,primary_key = True)
+    name : str
+    city : str
+    email : str
 
 users = {
     1 : {'id':1,
@@ -46,6 +49,10 @@ class UserUpdate(BaseModel):
     city : str
     email : str
 
+@app.on_event("startup")
+def on_startup():
+    creat_db_tables()
+
 @app.get("/") # @ - decorator , "get('/hello')"--> http://localhost:8000/hello
 def index(): # index is arbitary
     return {"Message" : "Hello World"}
@@ -56,13 +63,17 @@ def test():
     return {"Message" : "Testing World",
             "Value" : a}
 
-@app.get("/users")
+@app.get("/users", status_code=200)
 def get_users(x_api_key: Annotated[Union[str,None], Header()], city:str=None): # Annotated|str
-    if city is None or city == "":
-        return {"message": "Users List", "data": users, "header" : x_api_key}
-    else:
-        list = [x for x in users.values() if x.get('city').lower()==city.lower()]
-        return {"message": "Users List", "data": list, "filter":city, "header": x_api_key}
+    # if city is None or city == "":
+    #     return {"message": "Users List", "data": users, "header" : x_api_key}
+    # else:
+    #     list = [x for x in users.values() if x.get('city').lower()==city.lower()]
+    #     return {"message": "Users List", "data": list, "filter":city, "header": x_api_key}
+    with Session(engine) as session:
+        users = session.query(Users).all() # select * from users
+        print(f'users list {users}')
+        return {"message": "Users list", "data":users,"header":x_api_key}
 
 
 # @app.get("/users/city")
@@ -72,30 +83,63 @@ def get_users(x_api_key: Annotated[Union[str,None], Header()], city:str=None): #
 
 @app.get("/users/{user_id}") # GET baseURL/users/1
 def get_user_by_id(user_id:int):
-    return {"message": "Users List", "data": users[user_id]}
+    with Session(engine) as session:
+        user = session.query(Users).filter(User.id == user_id).one_or_none() 
+    # return {"message": "Users List", "data": users[user_id]}
+        return {"message": "Users Details", "data": user}
 
-@app.post("/users")
-def create_user(user : User):
-    # users[user.id] = user
-    users.update({user.id: dict(user)})
-    return {"message" : "New User Added", "Users": users}
+@app.post("/users",status_code=201)
+# def create_user(user : User):
+#     # users[user.id] = user
+#     users.update({user.id: dict(user)})
+#     return {"message" : "New User Added", "Users": users}
+def create_user(user:Users):
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return {"message":"New User", "data": user}
 
 @app.put("/users/{user_id}")
-def update_user(user_id:int, user:UserUpdate):
-    if user_id in users.keys():
-        updated_user = users.get(user_id)
-        updated_user.update({"city": user.city})  
-        updated_user.update({"email": user.email})
-        updated_user.update({"name": user.name})
-        users.update({user_id : updated_user})
-        return {"message" : "User Updated", "Users":users}
-    else:
-        return "User ID Doesn't Exist"
+# def update_user(user_id:int, user:UserUpdate):
+#     if user_id in users.keys():
+#         updated_user = users.get(user_id)
+#         updated_user.update({"city": user.city})  
+#         updated_user.update({"email": user.email})
+#         updated_user.update({"name": user.name})
+#         users.update({user_id : updated_user})
+#         return {"message" : "User Updated", "Users":users}
+#     else:
+#         return "User ID Doesn't Exist"
+def update_user(user_id:int, user:Users):
+    with Session(engine) as session:
+        user_exist = session.query(Users).filter(Users.id == user_id).one_or_none()
+        if not user_exist:
+            raise HTTPException(404, 'Invalid User ID')
+        
+        user_exist.name = user.name
+        user_exist.city = user.city
+        user_exist.email = user.email
+        session.add(user_exist)
+        session.commit()
+        session.refresh(user_exist)
+        return {"message":"User Updated Successfully","data":user_exist}
     
+
+
 @app.delete("/users/{user_id}")
+# def delete_user(user_id:int):
+#     if user_id in users.keys():
+#         del users[user_id]
+#         return {"message":"User Deleted","Users":users}
+#     else:
+#         return {"message":"User_id is not valid"}
 def delete_user(user_id:int):
-    if user_id in users.keys():
-        del users[user_id]
-        return {"message":"User Deleted","Users":users}
-    else:
-        return {"message":"User_id is not valid"}
+    with Session(engine) as session:
+        user_exist = session.query(Users).filter(Users.id == user_id).one_or_none()
+        if not user_exist:
+            raise HTTPException(404, 'Invalid User ID')
+
+        session.delete(user_exist)
+        session.commit()
+        return {"message":"User Delted Successfully", "data":user_exist}
